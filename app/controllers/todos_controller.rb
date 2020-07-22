7754db95bc2a336076892544f10374fae734569e
Jennifer Sall <jennifer.sall@gmail.com>
2020-07-22 13:14:23 -0400

    Fixed Todo Controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,56 +1,53 @@
 class TodosController < ApplicationController
-	before_action :set_todo, only: [:edit, :update, :show,"destroy"]
-	def new
-		@todo = Todo.new
-	end
-	
-	def create
-		@todo = Todo.find(params[:id])
-		if @todo.save
-			flash[:notice] = "Todo was created successfully!"
-		   redirect_to todo_path(@todo)
-		else 
-			render 'new'
-		end
-	end
-	
-	def show
-		
-	end
-	
-	def edit
-			
-	
-	end
-	
-	def update
-			
-			if 
-				flash[:notice] = "Todo was successfully updated"
-				redirect_to todo_path(@todo)
-			else 
-				render 'edit'
-			end
-	end
-	
-	def index
-		@todos = Todo.all
-	end
-	
-	def destroy
-		set_todo
-		@todo.destroy
-		flash[:notice] = "Todo was deleted successfully"
-		redirect_to todos_path
-	end
-		
-	private
-	
-		def set_todo
-			@todo = Todo.find(params[:id])
-		end
-	
-		def todo_params
-		params.require(:todo).permit(:name, :description)
-	end
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
+  
+  def new
+    @todo = Todo.new
+  end
+  
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      flash[:notice] = "Todo was created successfully!"
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was successfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @todos = Todo.all
+  end
+  
+  def destroy
+    @todo.destroy
+    flash[:notice] = "Todo was deleted successfully"
+    redirect_to todos_path
+  end
+  
+  private
+  
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+  
+    def todo_params
+      params.require(:todo).permit(:name, :description)
+    end
+
 end
